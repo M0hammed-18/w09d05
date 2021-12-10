d05350a921452a5f37f10a75a9ff7bf875062f80
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-10 18:07:51 +0300

    SingUpPageV1Done

--- src/components/SingUp/index.js
+++ src/components/SingUp/index.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import "./style.css";
+import { FaRegGrinTongue } from "react-icons/fa";
+import {BsFillEmojiHeartEyesFill } from "react-icons/bs";
 
-function SingUp() {
-    return (
-        <div>
-            Hi from SingUp
-        </div>
-    )
-}
 
-export default SingUp
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const Register = () => {
+  
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username,setName]=useState('')
+  const [role, setRole] = useState("61a7750f589f5a40c9c7848f");
+
+  const newuser = async () => {
+    try {
+      const result = await axios.post(`${BASE_URL}/singup`, {
+        username,
+        email,
+        password,
+        role
+      });
+      console.log(result);
+      navigate("/login")
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+    <div className="singUpPage">
+        <section className="sectionbox">
+
+      <h3 id="h3text"> SingUp </h3>
+      <input id="inputbox"
+        type="text"
+        name="name"
+        placeholder="Enter Your Name "
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input id="inputbox"
+        type="email"
+        name="email"
+        placeholder="Enter Your Email "
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input id="inputbox"
+        type="password"
+        name="password"
+        placeholder="Enter Your Password "
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button  id="buttonbox"onClick={newuser}>Regester </button>
+      </section>
+      </div>
+    </>
+  );
+};
+
+export default Register;
