5b1c407f2ea4c4823a06384f7009ac310e700533
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-10 18:45:57 +0300

    LoginPageV1Done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css"
 
-function Login() {
-    return (
-        <div>
-            Hi from Login
-        </div>
-    )
-}
 
-export default Login
+const Login = () => {
+  const navigate = useNavigate();
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [local, setLocal] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLocal(token);
+  }, []);
+
+  const login = async (e) => {
+      
+    try {
+        e.preventDefault();
+      const result = await axios.post(`${BASE_URL}/loginn`, {
+        email,
+        password,
+      });
+      console.log(result);
+      if(result.data.token){
+      localStorage.setItem("token", result.data.token);
+    
+      }      
+    } catch (err) {
+      console.log(err,"what");
+    }
+
+    // navigate("/");
+  };
+  return (
+    <div className="desing">
+     {!local?(
+       <div className="loginPage">
+
+      <section>
+      <h5>Login please</h5>
+      <input
+        type="email"
+        name="email"
+        placeholder="enter email "
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="enter password "
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={login} 
+      >Login</button>
+      </section>
+      </div>
+      ):(
+<h1></h1>
+      )}
+    </div>
+  );
+};
+
+export default Login;
