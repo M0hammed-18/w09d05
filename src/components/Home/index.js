78b7e2ba9719fc51a661e60d386e19af7eb0f683
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-10 13:09:01 +0300

    HomePageV1Done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SingUp from '../SingUp'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css"
+import { useNavigate } from "react-router-dom";
+import SingUp from "../SingUp";
+import Login from "../Login";
 
 
 function Home() {
-    return (
-        <div>
-<Link to={<SingUp/>}/>
-            Hi from home
+  const Navigate = useNavigate();
+
+  const register = () => {
+    Navigate("/signup");
+  };
+
+  const login = () => {
+    Navigate("/login");
+
+    
+  };
+  return (
+    //   <>
+    // <div className="HomePage">
+    //     <div className="headerimg">
+        
+      
+    //     </div>
+    // </div>
+    // </>
+    
+    <div className = "head-text">
+      <div className = "head-image">
+      <img
+          id="backgroundimg"
+          src="https://whatsnewinpublishing.com/wp-content/uploads/2019/10/Social_media_icons.jpg" alt="#"
+        /> 
+    
+      </div>
+        <div class='text-on-image'>
+        <h1 > Welcome To Social Media </h1>
+        <button  className="buttonHome" onClick={login} > Login </button>
+      <button  className="buttonHome" onClick={register} > Register </button>
+      
         </div>
-    )
+    </div>
+
+  );
 }
 
-export default Home
+export default Home;
+
+
+
+
+
